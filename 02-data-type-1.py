da134e9f086958ecacd644ae09aec959b01a905e
dytrack <sypark.tech@gmail.com>
2020-09-12 16:31:50 +0900

    Add 02-data-type-1.py
     - Lean int, float, str

---
+++ 02-data-type-1.py
@@ -0,0 +1,20 @@
+# 정수 int - integer
+print(1+1)
+# 소수 float
+print(1.5+2.3)
+#문자 str string
+print("hello")
+
+#문자열 더하기
+print("hello"+"1")
+
+#정수 소수 간 변경
+print(int(4/2))
+print(4//2)
+print(float(2))
+#print(str(3.2))
+
+# 문자열 숫자 간 정렬
+print("1")
+print(int("1"))
+print(int("1.5"))
