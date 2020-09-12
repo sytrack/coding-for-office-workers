dd9462c9790e545491c56bd4e5759af17c1ba481
sytrack <57398610+sytrack@users.noreply.github.com>
2020-09-12 16:14:23 +0900

    Add files via upload

---
+++ Conversation.py
@@ -0,0 +1,10 @@
+# 화면에 데이터 출력하기
+print(1+2)
+
+# 사용자로 부터 값 받기
+# 변수 활용
+name = input("what is your name? ")
+print("Hi, " + name)
+#print("안녕"+"하세요")
+name = input("당신의 이름은 무엇입니까?")
+print("넵, "+name)
